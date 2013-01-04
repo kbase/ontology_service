f4c78b8a0e9b3edd0bed6626ba5a4778e0e21e1a
U-BNL\feihe <feihe@maslov-dt2.bnl.gov>
2013-01-04 12:45:22 -0500

    Fei's minor change

--- lib/Bio/KBase/OntologyService/OntologyServiceImpl.pm
+++ lib/Bio/KBase/OntologyService/OntologyServiceImpl.pm
@@ -144,7 +144,7 @@ sub getGOIDList
         next if ! defined $domainMap{$data[2]};
         next if ! defined $ecMap{$data[3]};
         $g2idlist{$geneID} = [] if(! defined $g2idlist{$geneID}) ;
-        push $g2idlist{$geneID}, $data[0];
+        push $g2idlist{$geneID}, $data[0]."\t".$data[2]."\t".$data[3];
       } # end of fetch and counting
     } # end of types
 
@@ -222,7 +222,7 @@ GoID is a string
 
 =item Description
 
-For a given list of Features from a particular genome (for example "Athaliana") extract corresponding list of GO identifiers. This function call accepts six parameters: species name, a list of gene-identifiers, a list of ontology domains, a list of evidence codes, and lower & upper bound on the number of returned go-ids that a gene-id must have. The list of gene identifiers cannot be empty; however the list of ontology domains and the list of evidence codes can be empty. If any of the domain and the evidence-code lists is not empty then the gene-id and go-ids pairs retrieved from KBase are further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the initial results  then it is recommended to provide empty domain and evidence code lists. Finally, this function returns a mapping of only those gene-id to go-ids for which the count of go-ids per gene is between minimum and maximum count limit. Note that in the returned table of results, each gene-id is associated with a list of one of more go-ids. Also, a note on the input list: only one item per line is allowed.
+For a given list of Features from a particular genome (for example "Athaliana") extract corresponding list of GO identifiers. This function call accepts six parameters: species name, a list of gene-identifiers, a list of ontology domains, a list of evidence codes, and lower & upper bound on the number of returned go-ids that a gene-id must have. The list of gene identifiers cannot be empty; however the list of ontology domains and the list of evidence codes can be empty. If any of the domain and the evidence-code lists is not empty then the gene-id and go-ids pairs retrieved from KBase are further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the initial results  then it is recommended to provide empty domain and evidence code lists. Finally, this function returns a mapping of only those gene-id to go-ids for which the count of genes per go-id is between minimum and maximum count limit. Note that in the returned table of results, each gene-id is associated with a list of one of more go-ids. Also, a note on the input list: only one item per line is allowed.
 
 =back
 
@@ -337,13 +337,13 @@ sub getGoDesc
 
     my %go2desc = (); # gene to id list
     $results = \%go2desc;
-    my $pstmt = $dbh->prepare("select OntologyDescription from ontologies where OntologyID = ? and OntologyType = 'GO'");
+    my $pstmt = $dbh->prepare("select OntologyDescription, OntologyDomain from ontologies where OntologyID = ? and OntologyType = 'GO'");
     foreach my $goID (@{$goIDList}) {
 
       $pstmt->bind_param(1, $goID);
       $pstmt->execute();
       while( my @data = $pstmt->fetchrow_array()) {
-        $go2desc{$goID} = $data[0];
+        $go2desc{$goID} = $data[0]."\t".$data[1];
       } # end of fetch and counting
     } # end of types
     #END getGoDesc
@@ -430,6 +430,8 @@ GoDesc is a string
 
 For a given list of Features from a particular genome (for example "Athaliana" ) find out the significantly enriched GO terms in your feature-set. This function accepts five parameters: Species name, a list of gene-identifiers, a list of ontology domains, a list of evidence codes, and test type (e.g. "hypergeometric" and "chisq"). The list of gene identifiers cannot be empty; however the list of ontology domains and the list of evidence codes can be empty. If any of these two lists is not empty then the gene-id and the go-id pairs retrieved from KBase are further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the initial results then it is recommended to provide empty domain and evidence code lists. Final filtered list of the gene-id to go-ids mapping is used to calculate GO Enrichment using hypergeometric or chi-square test.
 
+Note that the current released verion ignore test type and by default, it uses hypergeometric test. So even if you do not provide TestType, it will do hypergeometric test.
+
 =back
 
 =cut
@@ -456,14 +458,17 @@ sub getGOEnrichment
     #BEGIN getGOEnrichment
     my $frst = getGOIDList($self, $sname, $geneIDList, $domainList, $ecList);
     my %ukey = ();
-    foreach my $geneID (keys %{$frst}) {
-      foreach my $goID (@{$frst->{$geneID}}) {
-        if(defined $ukey{$goID}) {
-          $ukey{$goID} = $ukey{$goID} + 1;
-        } else {
-          $ukey{$goID} = 1;
-        }
-      }
+   my @tem_goID=();
+	foreach my $geneID (keys %{$frst}) {
+		foreach my $mygoID (@{$frst->{$geneID}}) {
+	 	@tem_goID=split/\t/,$mygoID;
+		my $goID=$tem_goID[0];
+       		if(defined $ukey{$goID}) {
+          		$ukey{$goID} = $ukey{$goID} + 1;
+       		} else {
+          		$ukey{$goID} = 1;
+        	}
+      		}
     }
 
     $results = [];
@@ -472,11 +477,13 @@ sub getGOEnrichment
     my @goIDList = keys %ukey;
     my $rh_goDescList = getGoDesc($self, \@goIDList);
     my $rh_goID2Count = getGoSize($sname, \@goIDList, $domainList, $ecList);
+	my $wholeGeneSize;
     for(my $i = 0; $i <= $#goIDList; $i= $i+1) {
       my $goDesc = $rh_goDescList->{$goIDList[$i]};
       my $goSize = $rh_goID2Count->{$goIDList[$i]};
-      my $wholeGeneSize = 22000; # temporary... based on gene ID <-- need to be changed...
-      # calc p-value using any h.g. test
+	 $wholeGeneSize = 22000 if $sname=~/Ath/; # temporary... based on gene ID <-- need to be changed...
+	 $wholeGeneSize = 45000 if $sname=~/Ptr/;
+	 # calc p-value using any h.g. test
       my %rst = ();
       $rst{"pvalue"} = calculateStatistic(n11 => $ukey{$goIDList[$i]}, n1p => $geneSize, np1 => $goSize, npp => $wholeGeneSize);
       $rst{"goDesc"} = $goDesc;
@@ -577,8 +584,10 @@ terms in your feature-set. This function accepts seven parameters: Specie name, 
     evidence codes can be empty. If any of these two lists is not empty then the gene-id and the go-id pairs retrieved from KBase are 
     further filtered by using the desired ontology domains and/or evidence codes supplied as input. So, if you don't want to filter the 
     initial results then it is recommended to provide empty domain and evidence code lists. In any case, a mapping of only those 
-    gene-id to go-ids for which the count of go-ids per gene is between minimum and maximum count limit is carried forward. Final filtered 
+    gene-id to go-ids for which the count of genes per go-id is between minimum and maximum count limit is carried forward. Final filtered 
     list of the gene-id to go-ids mapping is used to calculate GO Enrichment using hypergeometric test.
+
+Note that the current released verion ignore test type and by default, it uses hypergeometric test. So even if you do not provide TestType, it will do hypergeometric test.
 
 =back
 
@@ -704,7 +713,7 @@ Plant Species names.
      Creinhardtii: Chlamydomonas reinhardtii
      Gmax: Glycine max
      Oglaberrima: Oryza glaberrima
-     Oindica: Oryza sativa indiaca
+     Oindica: Oryza sativa indica
      Osativa: Oryza sativa japonica
      Ptrichocarpa: Populus trichocarpa 
      Sbicolor: Sorghum bicolor 
@@ -900,7 +909,7 @@ a string
 
 =item Description
 
-Captures which branch of knowledge the GO terms refers to e.g. "Biological Process", "Molecular Function", "Cellular Process" etc.
+Captures which branch of knowledge the GO terms refers to e.g. "biological_process", "molecular_function", "cellular_component" etc.
 
 
 =item Definition
